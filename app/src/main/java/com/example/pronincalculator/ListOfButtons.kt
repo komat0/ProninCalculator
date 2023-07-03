738346e064a9df90f12d49226af1533529eba777
komat0 <111368527+komat0@users.noreply.github.com>
2023-07-03 23:14:16 +0300

    It Works Better!

---
+++ app/src/main/java/com/example/pronincalculator/ListOfButtons.kt
@@ -0,0 +1,4 @@
+package com.example.pronincalculator
+
+class ListOfButtons {
+}
--- app/src/main/java/com/example/pronincalculator/MainActivity.kt
+++ app/src/main/java/com/example/pronincalculator/MainActivity.kt
@@ -2,25 +2,45 @@ package com.example.pronincalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
-import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.example.pronincalculator.databinding.ActivityMainBinding
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var calculatorScreenText: EditText
+    /*
+    Для тестовой работы с Binding View. Вводим переменную с ссылкой на класс Binding, которая будет
+    хранить в себе все ссылки из активити и через нее можно будет получать доступ, в том числе к
+    ресурсам, для замены findViewById.
+    */
+    private lateinit var bindingClass: ActivityMainBinding
+
+    private lateinit var calculatorScreenText: TextView
     private lateinit var actionButtons: List<Button>
     private lateinit var digitalButtons: List<Button>
     private lateinit var buttonDivision: Button
     private lateinit var buttonMulti: Button
     private lateinit var buttonPlus: Button
     private lateinit var buttonMinus: Button
+    private lateinit var buttonDot: Button
+    private lateinit var darkModeSwitcher: SwitchMaterial
     private var digitalCollector: String = ""
     private var digitalMemory: String = ""
     private var actionButtonSign: Char = ' '
+    private var decimalClicked: Boolean = false
+    private var textViewValueCopyByTap = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        /* Использую LayoutInflater для работы BindingClass. По сути "наполнение". */
+//        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+//
+//        /* Для работы с доступом к ресурсам через BindingClass. */
+//        setContentView(bindingClass.root)
+
+        /* Для работы с классическим доступом к ресурсам. */
         setContentView(R.layout.activity_main)
 
         calculatorScreenText = findViewById(R.id.calculatorScreenText)
@@ -28,6 +48,7 @@ class MainActivity : AppCompatActivity() {
         buttonMulti = findViewById(R.id.buttonMulti)
         buttonPlus = findViewById(R.id.buttonPlus)
         buttonMinus = findViewById(R.id.buttonMinus)
+        buttonDot = findViewById(R.id.buttonDot)
 
         val button0: Button = findViewById(R.id.button0)
         val button1: Button = findViewById(R.id.button1)
@@ -47,63 +68,164 @@ class MainActivity : AppCompatActivity() {
         actionButtons = listOf(
             buttonDivision, buttonMulti, buttonPlus, buttonMinus
         )
-    }
 
-    fun isIntegerOrDecimal(str: String): Boolean {
-        return str.toIntOrNull() != null
+        darkModeSwitcher = findViewById(R.id.darkModeSwitch)
     }
 
     fun onClickAnyButton(view: View) {
         val buttonText = (view as Button).text.toString()
 
         when {
+            /*
+            Проверка что было завершено вычисление. Если так, то чистка все сохраненных переменные
+            */
             digitalButtons.contains(view) -> {
+                if (actionButtonSign == '=' && digitalCollector == "0.") {
+                    digitalMemory = ""
+                    actionButtonSign = ' '
+                    calculatorScreenText.text = digitalCollector
+                } else if (actionButtonSign == '=') {
+                    digitalMemory = ""
+                    digitalCollector = ""
+                    actionButtonSign = ' '
+                    calculatorScreenText.text = ""
+                }
+                /* При нажатии циферных кнопок, собирает число из последовательных нажатий на цифры и
+                отображает их на экране калькулятора. */
                 digitalCollector += buttonText
-                calculatorScreenText.setText(digitalCollector)
+                calculatorScreenText.text = digitalCollector
             }
-
+            /* Обработка кнокок вычисления. Если кнопка нажата, записывает введенное число в
+            digitalMemory и очищает digitalCollector для генерации второго числа.
+            Добавляет знак математического действия в actionButtonSign для дальнейшего использования
+            при нжатии на "=".
+            Отменяет флаг использования точки.
+            */
             actionButtons.contains(view) -> {
                 digitalMemory = digitalCollector
                 digitalCollector = ""
                 actionButtonSign = view.text.single()
+                decimalClicked = false
             }
-
+            /* Обработка кнопки "=".
+            В зависимости от знака в переменной actionButtonSign выполняются соответствующие действия.
+            Вычисления записываются в result типа Double. */
             buttonText.contains("=") -> {
-                var result: String = ""
+                var result = 0.0
 
                 when (actionButtonSign) {
-
                     '+' -> {
-                        result = (digitalMemory.toInt() + digitalCollector.toInt()).toString()
-                        calculatorScreenText.setText(result)
-                        digitalCollector = calculatorScreenText.toString()
+                        result = digitalMemory.toDouble() + digitalCollector.toDouble()
                     }
 
                     '-' -> {
-                        result = (digitalMemory.toInt() - digitalCollector.toInt()).toString()
-                        calculatorScreenText.setText(result)
-                        digitalCollector = calculatorScreenText.toString()
+                        result = digitalMemory.toDouble() - digitalCollector.toDouble()
                     }
 
                     '×' -> {
-                        result = (digitalMemory.toInt() * digitalCollector.toInt()).toString()
-                        calculatorScreenText.setText(result)
-                        digitalCollector = calculatorScreenText.toString()
+                        result = digitalMemory.toDouble() * digitalCollector.toDouble()
                     }
 
                     '÷' -> {
-                        result = (digitalMemory.toInt() / digitalCollector.toInt()).toString()
-                        calculatorScreenText.setText(result)
-                        digitalCollector = calculatorScreenText.toString()
+                        val divisor = digitalCollector.toDouble()
+                        result = if (divisor != 0.0) {
+                            digitalMemory.toDouble() / divisor
+                        } else {
+                            Toast.makeText(
+                                applicationContext,
+                                "Не пытайтесь делить на 0.",
+                                Toast.LENGTH_SHORT
+                            )
+                                .show()
+                            0.0
+                        }
                     }
                 }
-            }
 
+                val formattedResult = if (isInteger(result)) {
+                    result.toInt().toString()
+                } else {
+                    String.format("%.2f", result)
+                }
+
+                calculatorScreenText.text = formattedResult
+                digitalCollector = formattedResult
+                actionButtonSign = '='
+                decimalClicked = false
+
+            }
+            /* Обработка нажатия на кнопку "."
+            В случае нажатия, добавляет к числу и делает метку что было нажатие. Если было,
+            не добавляет вторую.
+            При нажатии первой, добавляет впереди "0". */
+            view == buttonDot -> {
+                if (!decimalClicked) {
+                    if (actionButtonSign == '=') {
+                        digitalMemory = ""
+                        digitalCollector = ""
+                        actionButtonSign = ' '
+                        calculatorScreenText.text = ""
+                    }
+
+                    if (digitalCollector.isEmpty()) {
+                        digitalCollector = "0."
+                    } else {
+                        digitalCollector += "."
+                    }
+                    calculatorScreenText.text = digitalCollector
+                    decimalClicked = true
+                }
+            }
+            /*
+            Обработка нажатия на кнопку сброса.
+            При нажатии обнуляются все переменные и сохраненные данные,
+            а также сбрасывается флаг decimalClicked
+            */
             buttonText.contains("C") -> {
                 digitalMemory = ""
                 digitalCollector = ""
-                calculatorScreenText.setText("0")
+                calculatorScreenText.text = "0"
                 actionButtonSign = ' '
+                decimalClicked = false
+            }
+        }
+    }
+
+    /*
+    Функция проверки строки на дестяичность. Если можно привести
+    к Integer - выдает True, если нет - False
+    */
+    private fun isInteger(number: Double): Boolean {
+        return number % 1 == 0.0
+    }
+
+    /*
+    Обработка нажатия на экран калькулятора. Данные с экрана сохраняются в переменную
+    textViewValueCopyByTap и выводится Тост с сообщением что аднные скопированы.
+    */
+    fun onClickTextView(view: View) {
+        textViewValueCopyByTap = calculatorScreenText.text.toString()
+        Toast.makeText(
+            applicationContext, "В память скопировано значение :$textViewValueCopyByTap",
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+    fun darkModeChannge(view: View) {
+        darkModeSwitcher.setOnClickListener {
+            if (darkModeSwitcher.isChecked) {
+                Toast.makeText(
+                    applicationContext,
+                    "Ночной режим Активирован!",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+            } else {
+                Toast.makeText(
+                    applicationContext,
+                    "Ночной режим Деактивирован!",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
     }
